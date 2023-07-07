706535a9e966cd0e85b0cdc208d7d63e5c92e116
Fay <faybeata.k@gmail.com>
2023-07-07 16:44:15 +0700

    change: improved ex. 2

--- puede-questions/Chapter_2/Exercise 2.cpp
+++ puede-questions/Chapter_2/Exercise 2.cpp
@@ -7,6 +7,14 @@ int main() {
 
     printf("Masukkan jumlah gaji: ");
     scanf("%i", &gaji);
+    
+    int l_100 = gaji / 100'000;
+    int l_50 = gaji % 100'000 / 50'000;
+    int l_20 = gaji % 50'000 / 20'000;
+    int l_10 = gaji % 50'000 % 20'000 / 10'000;
+    int l_5 = gaji % 10'000 / 5'000;
+    int l_2 = gaji % 10'000 % 5'000 / 2'000;
+    int l_1 = gaji % 5'000 % 2'000 / 1'000;
 
-    printf("100.000 = %i lembar\n50.000 = %i lembar\n20.000 = %i lembar\n10.000 = %i lembar\n5.000 = %i lembar\n2.000 = %i lembar\n1.000 = %i lembar", gaji / 100'000, gaji % 100'000 / 50'000, gaji % 50'000 / 20'000, gaji % 50'000 % 20'000 / 10'000, gaji % 10'000 / 5'000, gaji % 5'000 / 2'000, gaji % 5'000 % 2'000 / 1'000);
+    printf("100.000 = %i lembar\n50.000 = %i lembar\n20.000 = %i lembar\n10.000 = %i lembar\n5.000 = %i lembar\n2.000 = %i lembar\n1.000 = %i lembar",l_100,l_50,l_20,l_10,l_5,l_2,l_1);
 }
